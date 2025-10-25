b0e41256134c14e7fbe244f30c4ac49590271570
Manish Shivabhakti <manish.shivabhakti.99@gmail.com>
2025-10-25 20:01:54 +0545

    add some packages

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self' https:; script-src 'self' 'unsafe-eval' 'unsafe-inline' https:; style-src 'self' 'unsafe-inline' https:; img-src 'self' data: https: blob:; font-src 'self' data: https:; connect-src 'self' https: wss: ws:; worker-src 'self' blob:; frame-src 'self' https:;",
+            value: "default-src 'self' https:; script-src 'self' 'unsafe-eval' 'unsafe-inline' https:; style-src 'self' 'unsafe-inline' https:; img-src 'self' data: https: blob:; font-src 'self' data: https:; connect-src 'self' http://localhost:5001 http://127.0.0.1:5000 http://localhost:5001 http://127.0.0.1:5001 http://localhost:8000 http://127.0.0.1:8000 https: wss: ws:; worker-src 'self' blob:; frame-src 'self' https:;",
           },
           {
             key: 'X-Frame-Options',
